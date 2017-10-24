8d9e3ef943322dd837bb850ed3fdfef33c861422
Dan Untenzu <untenzu@webit.de>
2017-10-24 19:22:04 +0200

    [TASK] Set new version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,8 +24,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.4.0',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-0.0.0',
-            'typo3' => '7.6.0-7.6.99',
+            'php' => '5.5.0-7.1.0',
+            'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
